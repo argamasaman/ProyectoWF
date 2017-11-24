41500545b84cbd02201c8e175b50d49d663903da
Juan Matías García Lozano <juanmatiasgarcialozano@gmail.com>
2017-11-24 19:57:43 +0100

    Formulario de productos a falta de 3 botones

--- ProyectoWF/FrmListadoProductos.Designer.cs
+++ ProyectoWF/FrmListadoProductos.Designer.cs
@@ -3,14 +3,14 @@
     partial class FrmListadoProductos
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,19 +20,233 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.butAñadir = new System.Windows.Forms.Button();
+            this.butEditar = new System.Windows.Forms.Button();
+            this.butBorrar = new System.Windows.Forms.Button();
+            this.butCerrar = new System.Windows.Forms.Button();
+            this.listaProductos = new System.Windows.Forms.DataGridView();
+            this.busqueda = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listaProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(10);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(884, 661);
+            this.splitContainer1.SplitterDistance = 119;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCategoria);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(864, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Busqueda avanzada";
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(87, 63);
+            this.txtCategoria.Margin = new System.Windows.Forms.Padding(10);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(180, 20);
+            this.txtCategoria.TabIndex = 15;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(87, 23);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(10);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(180, 20);
+            this.txtNombre.TabIndex = 14;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Categoría";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nombre";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.butAñadir, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.butEditar, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.butBorrar, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.butCerrar, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listaProductos, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.busqueda, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 538);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // butAñadir
+            // 
+            this.butAñadir.Image = global::ProyectoWF.Properties.Resources.nuevo;
+            this.butAñadir.Location = new System.Drawing.Point(609, 10);
+            this.butAñadir.Margin = new System.Windows.Forms.Padding(10);
+            this.butAñadir.Name = "butAñadir";
+            this.butAñadir.Size = new System.Drawing.Size(75, 32);
+            this.butAñadir.TabIndex = 0;
+            this.butAñadir.UseVisualStyleBackColor = true;
+            // 
+            // butEditar
+            // 
+            this.butEditar.Image = global::ProyectoWF.Properties.Resources.editar;
+            this.butEditar.Location = new System.Drawing.Point(704, 10);
+            this.butEditar.Margin = new System.Windows.Forms.Padding(10);
+            this.butEditar.Name = "butEditar";
+            this.butEditar.Size = new System.Drawing.Size(75, 32);
+            this.butEditar.TabIndex = 1;
+            this.butEditar.UseVisualStyleBackColor = true;
+            // 
+            // butBorrar
+            // 
+            this.butBorrar.Image = global::ProyectoWF.Properties.Resources.eliminar;
+            this.butBorrar.Location = new System.Drawing.Point(799, 10);
+            this.butBorrar.Margin = new System.Windows.Forms.Padding(10);
+            this.butBorrar.Name = "butBorrar";
+            this.butBorrar.Size = new System.Drawing.Size(75, 32);
+            this.butBorrar.TabIndex = 2;
+            this.butBorrar.UseVisualStyleBackColor = true;
+            this.butBorrar.Click += new System.EventHandler(this.btBorrar_Click);
+            // 
+            // butCerrar
+            // 
+            this.butCerrar.Location = new System.Drawing.Point(799, 505);
+            this.butCerrar.Margin = new System.Windows.Forms.Padding(10);
+            this.butCerrar.Name = "butCerrar";
+            this.butCerrar.Size = new System.Drawing.Size(75, 23);
+            this.butCerrar.TabIndex = 3;
+            this.butCerrar.Text = "Cerrar";
+            this.butCerrar.UseVisualStyleBackColor = true;
+            // 
+            // listaProductos
+            // 
+            this.listaProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.listaProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.listaProductos, 4);
+            this.listaProductos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listaProductos.Location = new System.Drawing.Point(10, 62);
+            this.listaProductos.Margin = new System.Windows.Forms.Padding(10);
+            this.listaProductos.Name = "listaProductos";
+            this.listaProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.listaProductos.Size = new System.Drawing.Size(864, 423);
+            this.listaProductos.TabIndex = 4;
+            // 
+            // busqueda
+            // 
+            this.busqueda.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.busqueda.Image = global::ProyectoWF.Properties.Resources.busqueda;
+            this.busqueda.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.busqueda.Location = new System.Drawing.Point(10, 10);
+            this.busqueda.Margin = new System.Windows.Forms.Padding(10);
+            this.busqueda.Name = "busqueda";
+            this.busqueda.Size = new System.Drawing.Size(125, 32);
+            this.busqueda.TabIndex = 0;
+            this.busqueda.Text = "Abrir Busqueda";
+            this.busqueda.UseVisualStyleBackColor = true;
+            this.busqueda.Click += new System.EventHandler(this.busqueda_Click);
+            // 
+            // FrmListadoProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FrmListadoProductos";
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FrmListadoProductos";
+            this.Text = "Formulario de productos";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listaProductos)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button butAñadir;
+        private System.Windows.Forms.Button butEditar;
+        private System.Windows.Forms.Button butBorrar;
+        private System.Windows.Forms.Button butCerrar;
+        private System.Windows.Forms.DataGridView listaProductos;
+        private System.Windows.Forms.Button busqueda;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
+
--- ProyectoWF/FrmListadoProductos.cs
+++ ProyectoWF/FrmListadoProductos.cs
@@ -2,19 +2,143 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace ProyectoWF
 {
     public partial class FrmListadoProductos : Form
     {
+
+
+        SqlConnection con;
+        BindingSource bs;
+
+        DataTable table;
+        SqlDataAdapter adapter;
+        SqlCommand cmd;
+
         public FrmListadoProductos()
         {
             InitializeComponent();
+            splitContainer1.IsSplitterFixed = true;
+            con = Conexion.getConexion();
+            bs = new BindingSource();
+            cargar(listaProductos);
+
+            //Ocultar
+            listaProductos.Columns["ProductoID"].Visible = false;
+            listaProductos.Columns["Imagen"].Visible = false;
+            
         }
+
+        
+
+        public void cargar(DataGridView listaProductos)
+        {
+            try {
+                table = new DataTable("Productos");
+                adapter = new SqlDataAdapter("select * from Productos", con);
+                adapter.Fill(table);
+
+                listaProductos.DataSource = table;
+                bs.DataSource = table;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("No se pudo rellenar la lista..");
+            }
+            
+            
+        }
+
+        private void busqueda_Click(object sender, EventArgs e)
+        {
+            if (splitContainer1.Panel1Collapsed)
+            {
+                busqueda.Text = "Cerrar";
+                splitContainer1.Panel1Collapsed = false;
+                splitContainer1.SplitterDistance = 120;
+            }
+            else
+            {
+                busqueda.Text = "Abrir";
+                splitContainer1.Panel1Collapsed = true;
+
+            }
+
+           
+        }
+
+        
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            int iEntro = 0;
+            
+            switch (((TextBox)sender).Name)
+            {
+
+                case "txtNombre":
+                    iEntro = 1;
+                    bs.Filter = "ProductoNombre LIKE '%" + txtNombre.Text + "%'";
+                    break;
+                case "txtCategoria":
+                    iEntro = 2;
+                    bs.Filter = "CategoriaID LIKE '%" + txtCategoria.Text + "%'";
+                    break;
+                
+            }
+            if ((iEntro != 1) && (txtNombre.Text != ""))
+            {
+                bs.Filter = bs.Filter + " AND ProductoNombre LIKE '%" + txtNombre.Text + "%'";
+            }
+
+            if ((iEntro != 2) && (txtCategoria.Text != ""))
+            {
+                bs.Filter = bs.Filter + " AND CategoriaID LIKE '%" + txtCategoria.Text + "%'";
+            } 
+
+            listaProductos.DataSource = bs;
+        }
+
+        private void btBorrar_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            DataGridViewRow filaborrar;
+            int id;
+            DialogResult dr = MessageBox.Show("¿Esta seguro de borrar los datos seleccionados?", "Atencion",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (dr == DialogResult.Yes)
+            {
+                for (int i = 0; i < listaProductos.Rows.Count; i++)
+                {
+                    filaborrar = listaProductos.Rows[i];
+                    if (filaborrar.Selected == true)
+                    {
+                        id = (int)listaProductos.Rows[i].Cells["ProductoID"].Value;
+                        try
+                        {
+                            con.OpenAsync();
+                            cmd = new SqlCommand("DELETE FROM Productos WHERE ProductoID=" + id + "", con);
+                            count += cmd.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.ToString());
+                        }
+                    }
+                }
+                MessageBox.Show(count + " filas borradas", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cargar(listaProductos);
+            }
+        }
+
     }
 }
