406e452e5923f70dac7db79b7ce012300df1bae4
Juan Matías García Lozano <juanmatiasgarcialozano@gmail.com>
2017-12-14 19:07:08 +0100

    A falta de documentar

--- ProyectoWF/FrmListadoProductos.Designer.cs
+++ ProyectoWF/FrmListadoProductos.Designer.cs
@@ -137,6 +137,7 @@
             this.tableLayoutPanel1.Controls.Add(this.busqueda, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(800, 300);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
